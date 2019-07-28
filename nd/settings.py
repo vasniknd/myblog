2dfc96e0380eb0f924cdfc417d39935a249060a3
vasniknd <vasnik.nd@gmailcom>
2019-07-28 15:21:43 +0530

    change html

--- nd/settings.py
+++ nd/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'nd.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
